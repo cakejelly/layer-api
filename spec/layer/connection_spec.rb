6f68579f8c8b0c140214df45b06b6f2b955a727f
Jake Kelly <jake.kelly10@gmail.com>
2015-07-21 00:31:01 +0100

    Adds tests for run_request method

--- spec/layer/connection_spec.rb
+++ spec/layer/connection_spec.rb
@@ -4,6 +4,7 @@ describe Layer::Api::Connection do
   before do
     @layer = Layer::Api::Client.new
     @conn = @layer.connection
+    @default_headers = @layer.default_layer_headers.reject{|k, v| k == 'If-None-Match'}
   end
 
   describe ".connection" do
@@ -14,8 +15,7 @@ describe Layer::Api::Connection do
     it "should return a connection containing default layer headers" do
       # Remove If-None-Match header since it's always going to be random
       # and we can't compare it
-      default_headers = @layer.default_layer_headers.reject{|k, v| k == 'If-None-Match'}
-      expect(@conn.headers).to include(default_headers)
+      expect(@conn.headers).to include(@default_headers)
       expect(@conn.headers).to include("If-None-Match")
     end
 
@@ -27,6 +27,35 @@ describe Layer::Api::Connection do
     it "should re-use the same connection object" do
       new_conn = @layer.connection
       expect(@conn.object_id).to eq(new_conn.object_id)
+    end
+  end
+
+  describe ".run_request" do
+    it "should successfully add default layer headers to request" do
+      VCR.use_cassette('conversation') do
+        request = @layer.run_request(:get, 'users/test/blocks')
+        expect(request.env.request_headers).to include(@default_headers)
+        expect(request.env.request_headers).to include("If-None-Match")
+      end
+    end
+
+    it "should make request to url that is supplied as param" do
+      VCR.use_cassette('conversation') do
+        request_url = 'users/test/blocks'
+        request = @layer.run_request(:get, request_url)
+        request_actual = request.env.url.to_s.sub("#{@layer.base_url}/", "")
+
+        expect(request_actual).to eq (request_url)
+      end
+    end
+
+    it "should use the http method that is supplied as a param" do
+      VCR.use_cassette('announcement') do
+        method = :post
+        request = @layer.run_request(method, 'announcements', announcement_params)
+
+        expect(request.env.method).to eq(method)
+      end
     end
   end
 end
