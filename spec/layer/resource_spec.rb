6084d08fcccf22fb8fa20572584157c14b31ea46
Jake Kelly <jake.kelly10@gmail.com>
2016-02-16 22:05:15 +0000

    Fixes broken tests

--- spec/layer/resource_spec.rb
+++ spec/layer/resource_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Layer::Resource do
+  let(:client) { Layer::Platform::Client.new }
+
   describe ".class_name" do
     it "should return class name with module names omitted" do
       expected_name = "Resource"
@@ -28,35 +30,19 @@ describe Layer::Resource do
     end
   end
 
-  describe ".client" do
-    it "should return instance of Layer::Api::HttpClient" do
-      client = Layer::Resource.client
-
-      expect(client).to be_instance_of(Layer::HttpClient)
-    end
-  end
-
   describe "#new" do
     it "should assign attributes to the resource" do
       attributes = {one: "one", two: "two"}
-      resource = Layer::Resource.new(attributes)
+      resource = Layer::Resource.new(attributes, client)
 
       expect(resource.attributes).to eq(attributes)
-    end
-  end
-
-  describe "#client" do
-    it "should return instance of Layer::Api::HttpClient" do
-      client = Layer::Resource.new({}).client
-
-      expect(client).to be_instance_of(Layer::HttpClient)
     end
   end
 
   describe "#url" do
     it "should return url from attributes" do
       attributes = {"url" => "http://api.layer.com/url"}
-      url = Layer::Resource.new(attributes).url
+      url = Layer::Resource.new(attributes, client).url
 
       expect(url).to eq(attributes["url"])
     end
@@ -66,14 +52,14 @@ describe Layer::Resource do
     it "should return resource id without layer prefix" do
       uuid = "7f4a44f2-8f98-4990-88ea-1b7e6c087d4b"
       id = "layer:///conversations/#{uuid}"
-      resource = described_class.new({"id" => id})
+      resource = described_class.new({"id" => id}, client)
 
       expect(resource.uuid).to eq(uuid)
       expect(resource.id).to eq(id)
     end
 
     it "should return nil if no id exists" do
-      resource = described_class.new({})
+      resource = described_class.new({}, client)
 
       expect(resource.uuid).to be_nil
     end
@@ -84,14 +70,14 @@ describe Layer::Resource do
       it "should return the value for that key" do
         key = "key"
         value = "value"
-        resource = Layer::Resource.new({key => value})
+        resource = Layer::Resource.new({key => value}, client)
 
         expect(resource.key).to eq(value)
       end
     end
     context "when key doesn't exist in attributes" do
       it "should raise NoMethodError" do
-        resource = Layer::Resource.new({})
+        resource = Layer::Resource.new({}, client)
         expect{resource.blah}.to raise_error(NoMethodError)
       end
     end
